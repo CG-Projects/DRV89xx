f3f2264f8333a6ce499256d01d617dd763970b61
cg <c.g.mail@gmx.net>
2024-01-07 20:26:51 +0100

    add parallel motor support.

--- DRV89xx.cpp
+++ DRV89xx.cpp
@@ -2,62 +2,81 @@
 #include "DRV89xx.h"
 #include "DRV89xxRegister.h"
 
-DRV89xx::DRV89xx(int cs_pin, int fault_pin, int sleep_pin) :  _spi_settings(4000000, MSBFIRST, SPI_MODE1), _cs_pin(cs_pin), _fault_pin(fault_pin), _sleep_pin(sleep_pin) {}
+DRV89xx::DRV89xx(SPIClass &spi, int cs_pin, int fault_pin, int sleep_pin) : _spi_settings(4000000, MSBFIRST, SPI_MODE1), _spi(&spi), _cs_pin(cs_pin), _fault_pin(fault_pin), _sleep_pin(sleep_pin) {}
 
-void DRV89xx::begin() {
+void DRV89xx::begin()
+{
   Serial.println("DRV89xx begin called");
-  if (begin_called_) return;  // ignore duplicate begin calls
+  if (begin_called_)
+    return; // ignore duplicate begin calls
   begin_called_ = true;
   Serial.println("and run");
 
   // Setup SPI
-  SPI.begin(SCK, MISO, MOSI, _cs_pin);
-  SPI.setHwCs(true);
-  
+  _spi->begin(SCK, MISO, MOSI, _cs_pin);
+  _spi->setHwCs(false);
 
   // Setup pins
   pinMode(_cs_pin, OUTPUT);
-  if(_sleep_pin) {
+  digitalWrite(_cs_pin, HIGH);
+
+  if (_sleep_pin)
+  {
     pinMode(_sleep_pin, OUTPUT);
-    digitalWrite(_sleep_pin, HIGH);  // enable chip
+    digitalWrite(_sleep_pin, HIGH); // enable chip
   }
-  if(_fault_pin) pinMode(_fault_pin, INPUT);
+  if (_fault_pin)
+    pinMode(_fault_pin, INPUT);
 
   // Configure device
   _config_cache[(int)DRV89xxRegister::OLD_CTRL_1] = 0b11111111; // Disable open load detection on channels 1-8
   _config_cache[(int)DRV89xxRegister::OLD_CTRL_2] = 0b11001111; // Disable errors from open load detection, open load detect on channels 9-12
   _config_cache[(int)DRV89xxRegister::OLD_CTRL_3] = 0b10000000; // set Overcurrent protection to the most forgiving setting
-  _config_cache[(int)DRV89xxRegister::SR_CTRL_1] = 0b11111111; // Set slew rate to 2.5us vrs default 0.6us on half bridges (1-8)
-  _config_cache[(int)DRV89xxRegister::SR_CTRL_2] = 0b00001111; // Set slew rate to 2.5us vrs default 0.6us on half bridges (9-12)
-  _config_cache[(int)DRV89xxRegister::PWM_FREQ_CTRL] = 0xFF;  // Set all 4 PWM channels to 2kHz (max speed), TODO: Possibly make this a configurable option?
-  // TODO: Possibly make FW_CTRL_1+ an option?
+  _config_cache[(int)DRV89xxRegister::SR_CTRL_1] = 0b11111111;  // Set slew rate to 2.5us vrs default 0.6us on half bridges (1-8)
+  _config_cache[(int)DRV89xxRegister::SR_CTRL_2] = 0b00001111;  // Set slew rate to 2.5us vrs default 0.6us on half bridges (9-12)
+  _config_cache[(int)DRV89xxRegister::PWM_FREQ_CTRL] = 0x02;    // Set all 4 PWM channels to 200Hz 
+
   writeConfig();
 }
 
-void DRV89xx::configMotor(byte motor_id, byte hb1, byte hb2, byte pwm_channel, byte reverse_delay) {
+void DRV89xx::configMotor(byte motor_id, byte hb1, byte hb2, byte pwm_channel, byte reverse_delay, bool free_wheeling)
+{
   config_changed_ = true;
-  _motor[motor_id] = DRV89xxMotor(hb1, hb2, pwm_channel, reverse_delay);
+  _motor[motor_id] = DRV89xxMotor(hb1, hb2, pwm_channel, reverse_delay, free_wheeling);
 }
 
-byte DRV89xx::writeRegister(byte address, byte value) {
-  uint16_t ret = SPI.transfer16((address << 8) | value);
-  delayMicroseconds(1);  // Give the chip a chance to write
+byte DRV89xx::writeRegister(byte address, byte value)
+{
+  digitalWrite(_cs_pin, LOW);
+  uint16_t ret = _spi->transfer16((address << 8) | value);
+  digitalWrite(_cs_pin, HIGH);
+  delayMicroseconds(1); // Give the chip a chance to write
+  return ret;
 }
 
-byte DRV89xx::readRegister(byte address) {
-  uint16_t ret = SPI.transfer16(DRV89xx_REGISTER_READ | (address << 8));
+byte DRV89xx::readRegister(byte address)
+{
+  digitalWrite(_cs_pin, LOW);
+  uint16_t ret = _spi->transfer16(DRV89xx_REGISTER_READ | (address << 8));
+  digitalWrite(_cs_pin, HIGH);
   return ret & 0xFF;
 }
 
-
-void DRV89xx::readErrorStatus(bool print, bool reset) {
-  SPI.beginTransaction(_spi_settings);
-  if (digitalRead(_fault_pin) == 0) {
-    if (print) Serial.println("Error detected");
-  } else {
-    if (print) Serial.println("No DRV errors seen");
+void DRV89xx::readErrorStatus(bool print, bool reset)
+{  
+  _spi->beginTransaction(_spi_settings);
+  if (digitalRead(_fault_pin) == 0)
+  {
+    if (print)
+      Serial.println("Error detected");
   }
-  if (print) {
+  else
+  {
+    if (print)
+      Serial.println("No DRV errors seen");
+  }
+  if (print)
+  {
     Serial.print("Status: ");
     Serial.println(readRegister(0x00), HEX);
     Serial.print("Overcurrent: ");
@@ -73,38 +92,53 @@ void DRV89xx::readErrorStatus(bool print, bool reset) {
     Serial.print(" ");
     Serial.println(readRegister(0x04), HEX);
   }
-  
-  SPI.endTransaction();
-  if (digitalRead(_fault_pin) == 0 && reset) {  // reset
-    _config_cache[(int)DRV89xxRegister::CONFIG_CTRL] = 0b00000001;  // clear fault
-    writeConfig();  // try writing the config again, just in case
-    _config_cache[(int)DRV89xxRegister::CONFIG_CTRL] = 0b00000000;  // and go back to normal
-  } 
+
+  _spi->endTransaction();
+  if (digitalRead(_fault_pin) == 0 && reset)
+  {                                                                // reset
+    _config_cache[(int)DRV89xxRegister::CONFIG_CTRL] = 0b00000001; // clear fault
+    writeConfig();                                                 // try writing the config again, just in case
+    _config_cache[(int)DRV89xxRegister::CONFIG_CTRL] = 0b00000000; // and go back to normal
+  }
 }
 
-void DRV89xx::writeConfig() {
+void DRV89xx::writeConfig()
+{
   // Flush the 28 bytes of cache
-  SPI.beginTransaction(_spi_settings);
-  for(byte i=DRV89xx_CONFIG_WRITE_START; i<DRV89xx_CONFIG_BYTES; i++) {
+  _spi->beginTransaction(_spi_settings);
+  for (byte i = DRV89xx_CONFIG_WRITE_START; i < DRV89xx_CONFIG_BYTES; i++)
+  {
     DRV89xx::writeRegister(i, _config_cache[i]);
   }
-  SPI.endTransaction();
+  _spi->endTransaction();
 }
 
-void DRV89xx::updateConfig() {
-  if (!config_changed_) return;  // ignore duplicate writes
+void DRV89xx::updateConfig()
+{
+  if (!config_changed_)
+    return; // ignore duplicate writes
   config_changed_ = false;
   // Serial.println("Writing config update");
 
   readErrorStatus(false, true);
 
   byte i;
-  for(i=0;i<DRV89xx_MAX_MOTORS; i++) {
+  for (i = 0; i < DRV89xx_MAX_MOTORS; i++)
+  {
     _motor[i].applyConfig(_config_cache);
   }
-  SPI.beginTransaction(_spi_settings);
-  for(i=DRV89xx_UPDATE_START; i<=DRV89xx_UPDATE_END; i++) {
+  _spi->beginTransaction(_spi_settings);
+  for (i = DRV89xx_UPDATE_START; i <= DRV89xx_UPDATE_END; i++)
+  {
     DRV89xx::writeRegister(i, _config_cache[i]);
   }
-  SPI.endTransaction();
+  _spi->endTransaction();
+}
+
+void DRV89xx::clearFault()
+{
+  // Set CLR_FLT Bit to reset fault
+  _spi->beginTransaction(_spi_settings);
+  DRV89xx::writeRegister((byte)(DRV89xxRegister::CONFIG_CTRL), 0x01);
+  _spi->endTransaction();
 }
--- DRV89xx.h
+++ DRV89xx.h
@@ -2,7 +2,7 @@
   DRV89xx.h - Library for controlling brushed DC motors using a DRV89xx
   Currently written and tested for the ESP32 processor
   Created by Joseph Duchesne, September 23, 2020
-  Licensed under BSD 3 Clause 
+  Licensed under BSD 3 Clause
 */
 #ifndef DRV89xx_h
 #define DRV89xx_h
@@ -25,44 +25,108 @@
 class DRV89xx
 {
 
-  public:
-    DRV89xx(int cs_pin, int fault_pin, int sleep_pin);
+public:
+    DRV89xx(SPIClass &spi, int cs_pin, int fault_pin, int sleep_pin);
     void begin();
 
-    void configMotor(byte motor_id, byte hb1, byte hb2, byte pwm_channel, byte reverse_delay);
+    void configMotor(byte motor_id, byte hb1, byte hb2, byte pwm_channel, byte reverse_delay, bool free_wheeling);
     byte writeRegister(byte address, byte value);
     byte readRegister(byte address);
     void readErrorStatus(bool print, bool reset);
     void writeConfig();
     void updateConfig();
+    void clearFault();
 
-    void setMotor(byte motor, byte speed, byte direction){ 
-      config_changed_ = true;
-      _motor[motor].set(speed, direction); 
+    void setMotor(byte motor, byte speed, byte direction)
+    {
+        config_changed_ = true;
+        _motor[motor].set(speed, direction);
     };
-    void disableMotor(byte motor){ 
-      config_changed_ = true;
-      _motor[motor].disable();
-    };
-
-    void debugConfig() {
-      char buff[32];
-      for(int i=DRV89xx_CONFIG_WRITE_START; i<DRV89xx_CONFIG_BYTES; i++) {
-        sprintf(buff, "0x%02X: ", i);
-        Serial.print(buff);
-        for(int j = 7; j >= 0; j--) Serial.print(bitRead( _config_cache[i],j));
-        Serial.println();
-      }
+    void disableMotor(byte motor)
+    {
+        config_changed_ = true;
+        _motor[motor].disable();
     };
 
-    
-  private:
+    void debugConfig()
+    {
+        char buff[32];
+        for (int i = DRV89xx_CONFIG_WRITE_START; i < DRV89xx_CONFIG_BYTES; i++)
+        {
+            sprintf(buff, "0x%02X: ", i);
+            Serial.print(buff);
+            for (int j = 7; j >= 0; j--)
+                Serial.print(bitRead(_config_cache[i], j));
+            Serial.println();
+        }
+    };
+
+private:
+    SPIClass *_spi;
     SPISettings _spi_settings;
     int _cs_pin, _fault_pin, _sleep_pin;
     bool begin_called_ = false;
     bool config_changed_ = false;
-    byte _config_cache[DRV89xx_CONFIG_BYTES] = {0};  // Fully initalize the config cache as 0
+    byte _config_cache[DRV89xx_CONFIG_BYTES] = {0}; // Fully initalize the config cache as 0
     DRV89xxMotor _motor[DRV89xx_MAX_MOTORS];
 };
 
+class DRV89xxMotorP2
+{
+private:
+    DRV89xx *_driver;
+    int8_t _motor1 = -1, _motor2 = -1;
+
+public:
+    DRV89xxMotorP2(DRV89xx &driver) : _driver(&driver){};
+
+    void begin(int8_t motor1, int8_t motor2)
+    {
+        _motor1 = motor1;
+        _motor2 = motor2;
+    };
+
+    void set(byte speed, byte direction)
+    {
+        _driver->setMotor(_motor1, speed, direction);
+        _driver->setMotor(_motor2, speed, direction);
+    };
+
+    void disable()
+    {
+        _driver->disableMotor(_motor1);
+        _driver->disableMotor(_motor2);
+    };
+};
+
+class DRV89xxMotorP3
+{
+private:
+    DRV89xx *_driver;
+    int8_t _motor1 = -1, _motor2 = -1, _motor3 = -1;
+
+public:
+    DRV89xxMotorP3(DRV89xx &driver) : _driver(&driver){};
+
+    void begin(int8_t motor1, int8_t motor2, int8_t motor3)
+    {
+        _motor1 = motor1;
+        _motor2 = motor2;
+        _motor3 = motor2;
+    };
+
+    void set(byte speed, byte direction)
+    {
+        _driver->setMotor(_motor1, speed, direction);
+        _driver->setMotor(_motor2, speed, direction);
+        _driver->setMotor(_motor3, speed, direction);
+    };
+
+    void disable()
+    {
+        _driver->disableMotor(_motor1);
+        _driver->disableMotor(_motor2);
+        _driver->disableMotor(_motor3);
+    };
+};
 #endif
